1a8d29d4f220c7a5a875b41823cf4b942ce4a62e
therealteejay25 <therealteejay25@gmail.com>
2025-09-17 08:12:11 +0100

    Another commit

--- config/db.ts
+++ config/db.ts
@@ -1,11 +1,9 @@
 import mongoose from 'mongoose'
 
-export default function ConnectDB () {
-    mongoose.connect(process.env.MONOG_URI as string)
-.then(() => {
-    console.log('Monog DB Connected')
-})
-.catch((err) => {
-    console.error('MongoDB Error:', err)
-});
+const connectDB = async () => {
+    try {
+
+    } catch () {
+        
+    }
 }
